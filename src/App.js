8768c664bbba195922051973d155fe501b1a300f
sunilkapoor63 <118203785+sunilkapoor63@users.noreply.github.com>
2023-08-22 01:39:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Temp from './component/weather/temp';
+import MenuCard from './component/Basics/MenuCard';
+import Navbar from './component/Basics/Navbar';
+import Resturant from './component/Basics/Resturant';
+const App = () => {
+   return <Resturant/>
+};
+
+export default App
+
+
